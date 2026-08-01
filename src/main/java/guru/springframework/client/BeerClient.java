c06bbe252d8040a1dc290ff8716b72047348c531
Mikk.Sillamaa <mikk.sillamaa@rmit.ee>
2026-08-01 11:18:49 +0300

    Implemented endpoints

--- src/main/java/guru/springframework/client/BeerClient.java
+++ src/main/java/guru/springframework/client/BeerClient.java
@@ -2,6 +2,7 @@ package guru.springframework.client;
 
 import guru.springframework.model.BeerDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import tools.jackson.databind.JsonNode;
 
 import java.util.Map;
@@ -15,4 +16,12 @@ public interface BeerClient {
     Flux<JsonNode> listBeersJsonNode();
 
     Flux<BeerDTO> listBeerDtos();
+
+    Mono<BeerDTO> getBeerById(String id);
+
+    Flux<BeerDTO> getBeerByBeerStyle(String beerStyle);
+
+    Mono<BeerDTO> createBeer(BeerDTO beerDTO);
+
+    Mono<BeerDTO> updateBeer(BeerDTO beerDTO);
 }
--- src/main/java/guru/springframework/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/client/BeerClientImpl.java
@@ -4,6 +4,7 @@ import guru.springframework.model.BeerDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import tools.jackson.databind.JsonNode;
 
 import java.util.Map;
@@ -12,6 +13,7 @@ import java.util.Map;
 public class BeerClientImpl implements BeerClient {
 
     public static final String BEER_PATH = "/api/v1/beer";
+    public static final String BEER_PATH_ID = BEER_PATH + "/{beerId}";
     private final WebClient webClient;
 
     public BeerClientImpl(WebClient.Builder webClientBuilder) {
@@ -40,5 +42,45 @@ public class BeerClientImpl implements BeerClient {
     public Flux<BeerDTO> listBeerDtos() {
         return webClient.get().uri(BEER_PATH)
                 .retrieve().bodyToFlux(BeerDTO.class);
+    }
+
+    @Override
+    public Mono<BeerDTO> getBeerById(String id) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(BEER_PATH_ID)
+                        .build(id))
+                .retrieve()
+                .bodyToMono(BeerDTO.class);
+    }
+
+    @Override
+    public Flux<BeerDTO> getBeerByBeerStyle(String beerStyle) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(BEER_PATH)
+                        .queryParam("beerStyle", beerStyle).build())
+                .retrieve()
+                .bodyToFlux(BeerDTO.class);
+    }
+
+    @Override
+    public Mono<BeerDTO> createBeer(BeerDTO beerDTO) {
+        return webClient.post()
+                .uri(BEER_PATH)
+                .body(Mono.just(beerDTO), BeerDTO.class)
+                .retrieve()
+                .toBodilessEntity()
+                .flatMap(voidResponseEntity -> Mono.just(voidResponseEntity.getHeaders().get("Location").get(0)))
+                .map(path -> path.split("/")[path.split("/").length - 1])
+                .flatMap(this::getBeerById);
+    }
+
+    @Override
+    public Mono<BeerDTO> updateBeer(BeerDTO beerDTO) {
+        return webClient.put()
+                .uri(uriBuilder -> uriBuilder.path(BEER_PATH_ID).build(beerDTO.getId()))
+                .body(Mono.just(beerDTO), BeerDTO.class)
+                .retrieve()
+                .toBodilessEntity()
+                .flatMap(voidResponseEntity -> getBeerById(beerDTO.getId()));
     }
 }
